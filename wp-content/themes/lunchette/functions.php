dbac3bd798bb162e3078ba9c9b004f6d0670096d
DESKTOP-2QJH1HL\19 <ko.nagai.0801@gmail.com>
2024-04-12 14:10:28 +0900

    Add function

--- wp-content/themes/lunchette/functions.php
+++ wp-content/themes/lunchette/functions.php
@@ -1,8 +1,13 @@
-<?php //子テーマ用関数
+<?php
+//子テーマ用関数
 if ( !defined( 'ABSPATH' ) ) exit;
 
 //子テーマ用のビジュアルエディタースタイルを適用
 add_editor_style();
 
 //以下に子テーマ用の関数を書く
-add_filter('theme_description', '__return_false');
+add_filter('theme_description', 'remove_theme_description');
+
+function remove_theme_description() {
+    return '';
+}
