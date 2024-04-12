58469ad7af2005e4473c1ad5721e90784b1c06e8
DESKTOP-2QJH1HL\19 <ko.nagai.0801@gmail.com>
2024-04-12 13:09:21 +0900

    add theme

---
+++ wp-content/themes/cocoon-master/blocks/dist/blocks.build.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('lodash', 'react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-editor', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-keycodes', 'wp-primitives', 'wp-rich-text'), 'version' => '1ef60e53e825c2938559');
---
+++ wp-content/themes/cocoon-master/skins/skin-made-in-heaven/assets/js/block.js
@@ -0,0 +1,19 @@
+wp.blocks.registerBlockStyle('cocoon-blocks/timeline', {
+  name: 'hvn-timeline-mini',
+  label: 'ミニ'
+});
+
+wp.blocks.registerBlockStyle('cocoon-blocks/timeline', {
+  name: 'hvn-timeline-step',
+  label: 'ステップ'
+});
+
+wp.blocks.registerBlockStyle('cocoon-blocks/timeline', {
+  name: 'hvn-timeline-big',
+  label: 'ビッグ'
+});
+
+wp.blocks.registerBlockStyle('cocoon-blocks/timeline', {
+  name: 'hvn-timeline-box',
+  label: 'ボックス'
+});
