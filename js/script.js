bb9598853c9011c38a8bf55a86bd5acfaaadeec0
koh <ko.nagai.0801@gmail.com>
2024-05-06 17:24:57 +0900

    Edit script and css

--- js/script.js
+++ js/script.js
@@ -1,11 +1,16 @@
 let animation;
 
-// 初期化関数。ページのロード時に実行される
+// 初期化関数。ページのロード時に実行され、必要なセットアップを行います。
 function init() {
-  setupCanvas();
-  window.addEventListener('resize', setupCanvas);
+  setupCanvas();  // キャンバスを設定します
+  window.addEventListener('resize', setupCanvas);  // ウィンドウのリサイズに対応します
+  window.addEventListener('scroll', handleScroll);  // スクロールイベントに対応します
+  observeFadeInElements();  // フェードイン要素の監視を開始します
+  setupCollapseIconChanges();  // 折りたたみセクションのアイコン変更を設定します
+  addEventListeners();  // その他のイベントリスナーを追加します
 }
 
+// キャンバスの設定とアニメーションの初期化を行います。
 function setupCanvas() {
   const canvas = document.getElementById("waveCanvas");
   canvas.width = document.documentElement.clientWidth * 1.3;
@@ -21,7 +26,7 @@ function setupCanvas() {
   };
 
   if (animation) {
-    animation.kill();  // 既存のアニメーションを停止
+    animation.kill();  // 既存のアニメーションを停止します
   }
 
   animation = gsap.to(info, {
@@ -33,12 +38,12 @@ function setupCanvas() {
   });
 }
 
-// 波を描画する関数
+// キャンバス上に波形を描画するための関数です。
 function draw(canvas, info) {
   const context = canvas.contextCache;
   context.clearRect(0, 0, canvas.width, canvas.height);
 
-  var gradient = context.createLinearGradient(0, 0, 0, canvas.height);
+  let gradient = context.createLinearGradient(0, 0, 0, canvas.height);
   gradient.addColorStop(0, 'rgba(221,255,245, 1)');
 
   context.fillStyle = gradient;
@@ -50,13 +55,12 @@ function draw(canvas, info) {
   context.fill();
 }
 
-// サイン波を描画する関数
+// サイン波を描画します。
 function drawSine(canvas, info) {
   const context = canvas.contextCache;
   const xAxis = Math.floor(canvas.height / 2);
 
   context.moveTo(-10, xAxis);
-
   for (let i = -10; i <= canvas.width + 10; i += 10) {
     const x = (info.t + i) * info.frequency + info.phase;
     const y = Math.sin(x) * info.amplitude * info.unit;
@@ -64,168 +68,124 @@ function drawSine(canvas, info) {
   }
 }
 
-// スクロール位置に基づいてキャンバスの位置を変える関数
+// スクロールイベントを処理します。キャンバス位置の更新、コンテンツの透明度更新、透明度の更新を行います。
+function handleScroll() {
+  updateCanvasPosition();
+  updateContentOpacity();
+  updateOpacity();
+}
+
+// キャンバスの位置を更新します。画面幅993px以上では常に表示されません。
 function updateCanvasPosition() {
   const scrollY = window.scrollY;
   const heroSection = document.querySelector('.hero-section');
-  const triggerHeight = heroSection.offsetHeight;
+  const screenWidth = window.innerWidth;  // 画面の幅を取得
 
-  if (scrollY > triggerHeight) {
-    heroSection.style.position = 'fixed';
-    heroSection.style.top = `-${triggerHeight}px`;
-  } else {
+  // 993px以上の場合、ヒーローセクションを固定表示しないように設定
+  if (screenWidth >= 993) {
     heroSection.style.position = 'relative';
     heroSection.style.top = '0px';
+  } else {
+    // 993px未満の場合、スクロールに応じてヒーローセクションの位置を動的に変更
+    if (scrollY > heroSection.offsetHeight) {
+      heroSection.style.position = 'fixed';
+      heroSection.style.top = `-${heroSection.offsetHeight}px`;
+    } else {
+      heroSection.style.position = 'relative';
+      heroSection.style.top = '0px';
+    }
   }
 }
 
-// スクロール位置に基づいてコンテンツの透明度を更新する関数
+// スクロール位置に応じてコンテンツの透明度を更新します。
 function updateContentOpacity() {
   const scrollY = window.scrollY;
-  const triggerHeight = 2;
-  const maxScroll = 500;
-
-  const opacity = 1 - Math.min(1, (scrollY - triggerHeight) / (maxScroll - triggerHeight));
-
-  document.querySelector('.text-contents').style.opacity = opacity;
-  document.querySelector('.img-contents').style.opacity = opacity;
+  const opacity = 1 - Math.min(1, (scrollY - 2) / (500 - 2));  // 透明度を計算
+  document.querySelector('.text-contents').style.opacity = opacity;  // テキストコンテンツの透明度を設定
+  document.querySelector('.img-contents').style.opacity = opacity;  // 画像コンテンツの透明度を設定
 }
 
-// スクロール位置に基づいて透明度を更新する関数
+// スクロール位置に基づき、キャンバスと青色のキャンバスの透明度を更新します。
 function updateOpacity() {
   const scrollY = window.scrollY;
-
-  const waveCanvasTriggerHeight = 0;
-  const waveCanvasMaxScroll = 300;
-  const waveCanvasOpacity = Math.min(1, (scrollY - waveCanvasTriggerHeight) / (waveCanvasMaxScroll - waveCanvasTriggerHeight));
-
-  const blueCanvasTriggerHeight = 0;
-  const blueCanvasMaxScroll = 300;
-  const blueCanvasOpacity = Math.min(1, (scrollY - blueCanvasTriggerHeight) / (blueCanvasMaxScroll - blueCanvasTriggerHeight));
-
-  if (scrollY <= waveCanvasTriggerHeight) {
-    document.querySelector('#waveCanvas').style.opacity = 1;
-    animation.resume();
-  } else {
-    document.querySelector('#waveCanvas').style.opacity = waveCanvasOpacity;
-    document.querySelector('#blue-canvas').style.opacity = blueCanvasOpacity;
-    animation.pause();
-  }
+  const setOpacity = (triggerHeight, maxScroll) => Math.min(1, (scrollY - triggerHeight) / (maxScroll - triggerHeight));  // 透明度を計算する関数
+  document.querySelector('#waveCanvas').style.opacity = setOpacity(0, 300);  // 波のキャンバスの透明度を更新
+  document.querySelector('#blue-canvas').style.opacity = setOpacity(0, 300);  // 青色のキャンバスの透明度を更新
+  animation[scrollY <= 0 ? 'resume' : 'pause']();  // アニメーションを再開または一時停止
 }
 
-// 初期化関数を呼び出す
-init();
+// フェードイン要素の監視を行います。
+function observeFadeInElements() {
+  const fadeElements = document.querySelectorAll(".fd");
+  const observer = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add("fd-in");
+        observer.unobserve(entry.target);
+      }
+    });
+  });
+  fadeElements.forEach(element => observer.observe(element));
+}
 
-// ハンバーガーメニューのトグルを設定
-function toggleNavbar() {
-  var navbarToggler = document.querySelector('.navbar-toggler');
+// 折りたたみ要素のアイコン変更を設定します。
+function setupCollapseIconChanges() {
+  document.addEventListener('DOMContentLoaded', function () {
+    const toggleIcons = [
+      { id: 'clrtp-collapse', selector: 'a[href="#clrtp-collapse"] .wht-pls img', openImg: './img/plus.webp', closeImg: './img/cross.webp' },
+      { id: 'cns-collapse', selector: 'a[href="#cns-collapse"] .wht-pls img', openImg: './img/plus.webp', closeImg: './img/cross.webp' }
+    ];
+
+    toggleIcons.forEach(item => {
+      const element = document.getElementById(item.id);
+      const icon = document.querySelector(item.selector);
+
+      element.addEventListener('show.bs.collapse', function () {
+        icon.src = item.closeImg;
+        icon.alt = 'Close';
+      });
+      element.addEventListener('hide.bs.collapse', function () {
+        icon.src = item.openImg;
+        icon.alt = 'Open';
+      });
+    });
+  });
+}
+
+// ハンバーガーメニューのトグル機能とフッターの追従ボタン設定を行います。
+function addEventListeners() {
+  let navbarToggler = document.querySelector('.navbar-toggler');
   navbarToggler.addEventListener('click', function() {
     this.classList.toggle('toggled');
   });
-}
 
-// フェードイン効果を追加するためのIntersectionObserver
-const fadeElements = document.querySelectorAll(".fd");  // フェードイン対象の要素
-const observer = new IntersectionObserver((entries) => {
-  entries.forEach((entry) => {
-    if (entry.isIntersecting) {
-      entry.target.classList.add("fd-in");  // フェードインさせる
-      observer.unobserve(entry.target);  // 監視を停止
-    }
-  });
-});
-
-// フェードイン対象の要素を監視
-fadeElements.forEach((element) => {
-  observer.observe(element);  // フェードイン効果を監視
-});
-
-// What is …のトグル(+)->(×)へ
-document.addEventListener('DOMContentLoaded', function () {
-  // clrtp-collapseの設定
-  const clrtpCollapseElement = document.getElementById('clrtp-collapse');
-  const clrtpToggleIcon = document.querySelector('a[href="#clrtp-collapse"] .wht-pls img');
-
-  clrtpCollapseElement.addEventListener('show.bs.collapse', function () {
-    clrtpToggleIcon.src = './img/cross.webp';
-    clrtpToggleIcon.alt = 'Close';
-  });
-
-  clrtpCollapseElement.addEventListener('hide.bs.collapse', function () {
-    clrtpToggleIcon.src = './img/plus.webp';
-    clrtpToggleIcon.alt = 'Open';
-  });
-
-  // cns-collapseの設定
-  const cnsCollapseElement = document.getElementById('cns-collapse');
-  const cnsToggleIcon = document.querySelector('a[href="#cns-collapse"] .wht-pls img');
-
-  cnsCollapseElement.addEventListener('show.bs.collapse', function () {
-    cnsToggleIcon.src = './img/cross.webp';
-    cnsToggleIcon.alt = 'Close';
-  });
-
-  cnsCollapseElement.addEventListener('hide.bs.collapse', function () {
-    cnsToggleIcon.src = './img/plus.webp';
-    cnsToggleIcon.alt = 'Open';
-  });
-});
-
-// フッター追従ボタンの設定
-// ファーストビュー通過までは非表示
-document.addEventListener('DOMContentLoaded', function() {
   const heroSection = document.querySelector('.hero-section');
   const footerButton = document.querySelector('.fixed-bottom');
-
-  function checkHeroSectionPosition() {
+  const checkHeroSectionPosition = () => {
     const heroSectionBottom = heroSection.getBoundingClientRect().bottom;
-    
-    if (window.scrollY > heroSectionBottom) {
-      // ヒーローセクションがビューポートから外れたらフェードイン
-      footerButton.style.opacity = 1;
-    } else {
-      // ヒーローセクションがまだビューポート内にある場合はフェードアウト
-      footerButton.style.opacity = 0;
-    }
-  }
+    footerButton.style.opacity = window.scrollY > heroSectionBottom ? 1 : 0;
+  };
 
-  // スクロールとリサイズイベントで位置をチェック
   window.addEventListener('scroll', checkHeroSectionPosition);
   window.addEventListener('resize', checkHeroSectionPosition);
-
-  // 初期読み込み時にも位置をチェック
   checkHeroSectionPosition();
-});
+}
 
-// Privacy Policy Copyrightと重ならないように調整
-document.addEventListener('DOMContentLoaded', function() {
+// フッターの高さに基づいてページ下部のバーのパディングを調整する機能
+function adjustFooterPadding() {
   const footer = document.querySelector('.footer');
   const fixedBottom = document.querySelector('.container-fluid.fixed-bottom');
+  const footerVisible = footer.getBoundingClientRect().top < window.innerHeight;
 
-  function adjustPadding() {
-    const footerHeight = footer.offsetHeight;
-    const footerRect = footer.getBoundingClientRect();
-    const footerVisible = footerRect.top < window.innerHeight;
-
-    if (footerVisible) {
-      fixedBottom.style.paddingBottom = `${footerHeight}px`;
-    } else {
-      fixedBottom.style.paddingBottom = '0px';
-    }
-  }
-
-  // 初期設定
-  adjustPadding();
-
-  // スクロールやウィンドウリサイズ時に調整
-  window.addEventListener('scroll', adjustPadding);
-  window.addEventListener('resize', adjustPadding);
-});
-
-// ハンバーガーメニューのトグル
-function toggleNavbar() {
-  var navbarToggler = document.querySelector('.navbar-toggler');
-  navbarToggler.addEventListener('click', function() {
-    this.classList.toggle('toggled');
-  });
+  fixedBottom.style.paddingBottom = footerVisible ? `${footer.offsetHeight}px` : '0px';
 }
+
+// イベントリスナーにフッターのパディング調整機能を追加
+window.addEventListener('scroll', adjustFooterPadding);
+window.addEventListener('resize', adjustFooterPadding);
+
+// 初期設定で一度実行しておく
+adjustFooterPadding();
+
+
+init();  // 初期化関数を呼び出します。
